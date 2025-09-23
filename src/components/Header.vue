3be8190aae3d164f1570156a40fa283a02504f4c
ArturKarim <starhunter73@ymail.com>
2025-09-23 15:15:35 +0400

    add Header components

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+<style>
+.header {
+  background-color: #193347;
+  max-width: 1440px;
+}
+</style>
+<script setup></script>
